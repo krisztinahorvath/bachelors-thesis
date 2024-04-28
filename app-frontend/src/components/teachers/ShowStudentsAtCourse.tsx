5423d878252866197df4f7f255ea6adf3dd2cc74
krisztinahorvath <91715582+krisztinahorvath@users.noreply.github.com>
2024-04-28 18:01:50 +0300

    added show all students at a course

--- app-frontend/src/components/teachers/ShowStudentsAtCourse.tsx
+++ app-frontend/src/components/teachers/ShowStudentsAtCourse.tsx
@@ -7,11 +7,15 @@ import { BACKEND_URL } from "../../constants";
 import { displayErrorMessage } from "../ToastMessage";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import {
   CircularProgress,
   Container,
   IconButton,
-  TextField,
+  Pagination,
+  Paper,
+  Stack,
+  TableContainer,
   Tooltip,
 } from "@mui/material";
 
@@ -19,11 +23,15 @@ interface ShowStudentsAtCourseProps {
   courseId: any; // Define the courseId prop
 }
 
+let page = 1;
 export const ShowStudentsAtCourse: React.FC<ShowStudentsAtCourseProps> = ({
   courseId,
 }) => {
   const [loading, setLoading] = useState(false);
   const [students, setStudents] = useState<Student[]>([]);
+
+  // const pageSize = 10;
+  const [noOfPages, setNoOfPages] = useState(0);
 
   useEffect(() => {
     setLoading(true);
@@ -51,91 +59,109 @@ export const ShowStudentsAtCourse: React.FC<ShowStudentsAtCourseProps> = ({
     );
   }
 
+  const reloadData = () => {
+    console.log(page);
+    setLoading(true);
+    // Promise.all([
+    //   fetch(
+    //     `${BACKEND_URL}/books/?pageNumber=${page - 1}&pageSize=${pageSize}`
+    //   ).then((response) => response.json()),
+    //   fetch(
+    //     `${BACKEND_URL}/books/count-authors?pageNumber=${
+    //       page - 1
+    //     }&pageSize=${pageSize}`
+    //   ).then((response) => response.json()),
+    // ]).then(([data, count]) => {
+    //   setBooks(data);
+    //   setNrAuthors(count);
+    //   setLoading(false);
+    // });
+  };
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    page = value;
+    reloadData();
+  };
+
   return (
     <Container>
       <h2>All enrolled students</h2>
 
       {loading && <CircularProgress />}
-      {!loading && students.length === 0 && <p>No books found</p>}
+      {!loading && students.length === 0 && (
+        <p>No students enrolled at course.</p>
+      )}
+
       {!loading && (
-        <IconButton component={Link} sx={{ mr: 3 }} to={`/books/add`}>
-          <Tooltip title="Add a new book" arrow>
+        <IconButton component={Link} sx={{ mr: 3 }} to={`/students/add`}>
+          <Tooltip title="Add a new student" arrow>
             <AddIcon color="primary" />
           </Tooltip>
         </IconButton>
       )}
 
-      {/* {!loading && (
-        <Button
-          variant={path.startsWith("/books/filter-year") ? "outlined" : "text"}
-          to="/books/filter-year"
-          component={Link}
-          color="inherit"
-          sx={{ mr: 5 }}
-          startIcon={<ViewListIcon />}
-        >
-          Filter
-        </Button>
-      )} */}
-
-      {/* {!loading && (
-        <Button
-          variant={
-            path.startsWith("/books/order-by-author-age") ? "outlined" : "text"
-          }
-          to="/books/order-by-author-age"
-          component={Link}
-          color="inherit"
-          sx={{ mr: 5 }}
-          startIcon={<ViewListIcon />}
-        >
-          Books ordered by average author age
-        </Button>
-      )} */}
-
-      {/* {!loading && (
-				<Button
-					variant={path.startsWith("/books/add-authors") ? "outlined" : "text"}
-					to="/books/add-authors"
-					component={Link}
-					color="inherit"
-					sx={{ mr: 5 }}
-					startIcon={<ViewListIcon />}>
-					Add authors to book
-				</Button> 
-			)} */}
-
       {!loading && students.length > 0 && (
-        <div>
-          {students.map((student, index) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginBottom: "8px",
-              }}
-            >
-              <TextField
-                label="Name"
+        <Container>
+          <TableContainer component={Paper}>
+            <Table>
+              <Thead>
+                <Tr style={{ backgroundColor: "#f5f5f5" }}>
+                  <Th align="center">#</Th>
+                  <Th align="center">Name</Th>
+                  <Th align="center">Unique Code</Th>
+                  <Th align="center">Email</Th>
+                  <Th align="center">Action</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {students.map((student, index) => (
+                  <Tr key={(page - 1) * 10 + index + 1}>
+                    <Td>{(page - 1) * 10 + index + 1}</Td>
+                    <Td>
+                      <Link
+                        to={`/books/${student.id}/details`}
+                        title="View student profile"
+                      >
+                        {student.name}
+                      </Link>
+                    </Td>
+                    <Td align="right">{student.uniqueIdentificationCode}</Td>
+                    <Td align="right">{student.email}</Td>
+                    <Td align="right">
+                      <IconButton onClick={() => handleDelete(student.id)}>
+                        <DeleteIcon sx={{ color: "red" }} />
+                      </IconButton>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+
+          <Container
+            style={{
+              backgroundColor: "white",
+              borderRadius: 10,
+              width: "100%",
+              maxWidth: 500,
+              margin: "0 auto",
+            }}
+          >
+            <Stack spacing={2}>
+              <Pagination
+                count={noOfPages}
+                page={page}
+                onChange={handlePageChange}
+                size="large"
                 variant="outlined"
-                value={student.name}
-                disabled
-                style={{ marginRight: "8px" }}
+                color="secondary"
               />
-              <TextField
-                label="Email"
-                variant="outlined"
-                value={student.email}
-                disabled
-                style={{ marginRight: "8px" }}
-              />
-              <IconButton onClick={() => handleDelete(student.id)}>
-                <DeleteIcon />
-              </IconButton>
-            </div>
-          ))}
-        </div>
+            </Stack>
+          </Container>
+        </Container>
       )}
     </Container>
   );
