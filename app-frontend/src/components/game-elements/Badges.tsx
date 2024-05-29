861d916a0e5ab05447c1bed109fb69fc98e2d5e1
krisztinahorvath <91715582+krisztinahorvath@users.noreply.github.com>
2024-05-29 23:03:09 +0300

    showing badges for having a certain amount of points without on time badge

--- app-frontend/src/components/game-elements/Badges.tsx
+++ app-frontend/src/components/game-elements/Badges.tsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Grid, Typography } from "@mui/material";
 import Airplane from "../../assets/airplane.svg";
 import Origami from "../../assets/origami.svg";
@@ -7,6 +7,11 @@ import Astronaut from "../../assets/astronaut.svg";
 import Lightbulb from "../../assets/lightbulb.svg";
 import MarioMushroom from "../../assets/mariomushroom.svg";
 import Clock from "../../assets/clock.svg";
+import { getShowPoints } from "../../utils/student-user-preferences";
+import { getToken } from "../../utils/auth-utils";
+import axios from "axios";
+import { BACKEND_URL } from "../../constants";
+import { displayErrorMessage } from "../ToastMessage";
 
 const AirplaneBadge = () => {
   return <img src={Airplane} alt="airplane" style={{ width: "35%" }} />;
@@ -50,13 +55,13 @@ const icons: {
     icon: <AirplaneBadge />,
     description: "Intermediate",
     achievementCriteria: "Unlocked at 600XP",
-    achieved: true,
+    achieved: false,
   },
   3: {
     icon: <LightbulbBadge />,
     description: "Advanced",
     achievementCriteria: "Unlocked at 30XP",
-    achieved: true,
+    achieved: false,
   },
   4: {
     icon: <MarioMushroomBadge />,
@@ -75,7 +80,7 @@ const icons: {
     description: "Always on time",
     achievementCriteria:
       "Badge unlocked after turning in 75% of assignments before the deadline.",
-    achieved: true,
+    achieved: false,
   },
 };
 
@@ -109,18 +114,70 @@ export const StudentAchievements: React.FC<{ courseData: any }> = ({
   //   const courseData = location.state;
 
   const [loading, setLoading] = useState(false);
-  const [finalGrade, setFinalGrade] = useState(0);
+  const [pointsVisibility, setPointsVisibility] = useState(false);
+  const [level, setLevel] = useState(-1);
+  const [finalGradeData, setFinalGradeData] = useState({
+    finalGrade: 0,
+    experiencePoints: 0,
+  });
+
+  const computeLevel = (grade: number) => {
+    if (grade < 2) {
+      setLevel(1);
+    } else if (grade >= 2 && grade < 4) {
+      setLevel(2);
+    } else if (grade >= 4 && grade < 6.5) {
+      setLevel(3);
+    } else if (grade >= 6.5 && grade < 9.5) {
+      setLevel(4);
+    } else setLevel(5);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    if (getShowPoints() === "true") setPointsVisibility(true);
+    const headers = { headers: { Authorization: `Bearer ${getToken()}` } };
+    axios
+      .get(
+        `${BACKEND_URL}/students/student-grades-at-course/${courseData.id}`,
+        headers
+      )
+      .then((response) => {
+        setFinalGradeData(response.data);
+        computeLevel(response.data.finalGrade);
+        setLoading(false);
+      })
+      .catch((error: any) => {
+        setLoading(false);
+        if (error.response) {
+          const errorMessage = error.response.data;
+          displayErrorMessage(errorMessage);
+        } else {
+          setLoading(false);
+          displayErrorMessage(
+            "An error occurred while fetching the assignments."
+          );
+        }
+      });
+  }, []);
 
   return (
     <>
       <h2>Achievements at {courseData.name}</h2>
-      <Container sx={{ marginTop: "5%" }}>
-        <Grid container spacing={3}>
-          {Object.keys(icons).map((key) => (
-            <IconWithDescription key={key} iconData={icons[key]} />
-          ))}
-        </Grid>
-      </Container>
+      {!loading && (
+        <Container sx={{ marginTop: "5%" }}>
+          <Grid container spacing={3}>
+            {Object.keys(icons).map((key) => {
+              const achieved = level >= parseInt(key);
+              const iconData = {
+                ...icons[key],
+                achieved: achieved,
+              };
+              return <IconWithDescription key={key} iconData={iconData} />;
+            })}
+          </Grid>
+        </Container>
+      )}
     </>
   );
 };
